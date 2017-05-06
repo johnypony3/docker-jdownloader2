776f485f6d61b7855a4d88e45e9c59754f9d8dff
johnypony3 <edward@theemm.com>
2017-05-06 04:22:55 -0700

    wip

--- firstrun.sh
+++ firstrun.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-  mkdir -p /config/Downloads
+mkdir -p /config/Downloads
 
-  if [ ! -d /config/jd2 ]; then
-    tar -zxvf /edward/jd2.tar -C /config/
-  fi
-  chown -R edward:users /config
-  chmod -R g+rw /config
+if [ ! -d /config/jd2 ]; then
+  tar -zxvf /root/jd2.tar -C /config/
+fi
+
+chown -R root:users /config
+chmod -R g+rw /config
